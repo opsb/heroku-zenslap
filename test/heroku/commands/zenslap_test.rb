d9dfd30f84f88db3857e7bc3d4dd2b55a5eb91d4
opsb <oliver@opsb.co.uk>
2010-09-24 15:01:03 +0100

    Added webmock. Refactored test to use mocha test spies

--- test/heroku/commands/zenslap_test.rb
+++ test/heroku/commands/zenslap_test.rb
@@ -4,10 +4,14 @@ class ZenslapTest < Test::Unit::TestCase
   context "zenslap command" do
     setup do
       @command = Heroku::Command::Zenslap.new nil
+      @command.stubs(:github_url).returns("git@github.com:opsb/zenslap-heroku")
+      RestClient.stubs(:post).with("http://zenslap.heroku.com/heroku/resources", :github_url => "git@github.com:opsb/zenslap-heroku")
+      @command.add      
     end
     should "provision continuous integration through zenslap API" do
-      @command.expects(:github_url).returns("git@github.com:opsb/zenslap-heroku")
-      @command.add
+      assert_received RestClient, :post do |expect| 
+        expect.with "http://zenslap.heroku.com/heroku/resources", :github_url => "git@github.com:opsb/zenslap-heroku" 
+      end
     end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,12 @@ require 'rubygems'
 require 'test/unit'
 require 'shoulda'
 require 'mocha'
+require 'bourne'
+require 'webmock/test_unit'
+
+class Test::Unit::TestCase
+  include WebMock
+end
 
 require 'lib/heroku/commands/zenslap.rb'
 
