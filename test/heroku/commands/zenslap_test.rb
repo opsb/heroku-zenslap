c86a4c16c4efc9f6cfc32f6be52944336a0d0a34
opsb <oliver@opsb.co.uk>
2010-10-01 18:12:57 +0100

    Formatting

--- test/heroku/commands/zenslap_test.rb
+++ test/heroku/commands/zenslap_test.rb
@@ -1,17 +1,24 @@
 require 'test_helper.rb'
 
 class ZenslapTest < Test::Unit::TestCase
-  context "zenslap command" do
+  
+  context "zenslap add command" do
+    
     setup do
       @command = Heroku::Command::Zenslap.new nil
       @command.stubs(:github_url).returns("git@github.com:opsb/zenslap-heroku")
-      RestClient.stubs(:post).with("http://zenslap.heroku.com/heroku/resources", :github_url => "git@github.com:opsb/zenslap-heroku")
+      RestClient.stubs(:post).with "http://zenslap.heroku.com/heroku/resources", 
+                                   :github_url => "git@github.com:opsb/zenslap-heroku"
       @command.add      
     end
+    
     should "provision continuous integration through zenslap API" do
       assert_received RestClient, :post do |expect| 
-        expect.with "http://zenslap.heroku.com/heroku/resources", :github_url => "git@github.com:opsb/zenslap-heroku" 
+        expect.with "http://zenslap.heroku.com/heroku/resources", 
+                    :github_url => "git@github.com:opsb/zenslap-heroku" 
       end
     end
+    
   end
+  
 end
