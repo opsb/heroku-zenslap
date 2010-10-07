54722a25300221e1f6f733ad3c478e06802e6b3e
opsb <oliver@opsb.co.uk>
2010-10-07 23:44:13 +0100

    Added progress reports during provisioning process

--- lib/heroku/commands/zenslap.rb
+++ lib/heroku/commands/zenslap.rb
@@ -8,12 +8,20 @@ module Heroku::Command
       git_repo = Git.new
       heroku_credentials = Heroku::Command::Auth.new(nil).get_credentials
       heroku_client = Heroku::Client.new *heroku_credentials
+      
+      puts "---> Installing zenslap addon"
       heroku_client.install_addon git_repo.heroku_app, "zenslap"
       zenslap_client = ZenslapClient.new
       zenslap_id = heroku_client.config_vars(git_repo.heroku_app)["ZENSLAP_ID"]
+      
+      puts "---> Configuring zenslap"
       zenslap_client.configure( zenslap_id, git_repo.github_url )
       github_client = GithubClient.new( git_repo.github_url, git_repo.github_credentials )
+      
+      puts "---> Adding github service hook"
       github_client.add_service_hook "http://zenslap.me/pushes"
+      
+      puts "---> Adding zenslap as github collaborator"
       github_client.add_collaborator( "zenslap" )
     end
 
