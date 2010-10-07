bd87eff275965d7965ac28cc134b98ffc0332526
opsb <oliver@opsb.co.uk>
2010-10-07 13:07:06 +0100

    Removed dead code

--- lib/heroku/commands/zenslap.rb
+++ lib/heroku/commands/zenslap.rb
@@ -31,90 +31,14 @@ module Heroku::Command
       github_client.add_collaborator( "zenslap" )
     end
 
-    # def add
-    #   puts "---> Checking availability"
-    #   if plugin_available?
-    #     provision_plugin
-    #   else
-    #     show_introduction
-    #   end
-    # end
-    # 
     # def plugin_available?
     #   heroku_client.addons.map{ |addon| addon["name"] }.find do |name| 
     #     name =~ /zenslap.*/
     #   end
-    # end    
-    # 
+    # end
+
     # def show_introduction
     #   puts "zenslap is the easiest way to add continuous integration to your heroku app. We're currently in alpha. You can go ahead and request an invitation@zenslap.me."
-    # end    
-    # 
-    # def provision_plugin
-    #   heroku_client.install_addon heroku_app, "zenslap"
-    #   zenslap_id = heroku_client.config_vars(heroku_app)["ZENSLAP_ID"]
-    #   zenslap_client.configure zenslap_id, { :github_url => github_url }
-    #   # ---> github_client.add_service_hook "http://zenslap.me/pushes"
-    #   # ---> github_client.add_collaborator "zenslap"
-    #   # ---> heroku_client.add_collaborator heroku_test_app, "admin@zenslap.me"
-    #   
-    #   
-    #   
-    # 
-    # 
-    #   puts "---> Configuring for #{github_url}"
-    #   RestClient.put "http://zenslap.me/heroku/resources/#{zenslap_id}", :repository => { :github_url => github_url }
-    # 
-    #   puts "---> Adding service hooks to github"
-    #   repo = Repository.new(github_url)
-    #   repo.add("http://zenslap.me/pushes")
-    # 
-    #   puts "---> Adding zenslap access to #{github_url}"
-    #   repo.add_github_access
-    #   
-    #   puts "---> Adding zenslap access to test environment"
-    #   heroku_client.add_collaborator heroku_test_app_name, "admin@zenslap.me"
-    # 
-    #   puts "---> Zenslap is ready. Your next push to github will be tested and you will be emailed the results."
-    # end    
-    # 
-    # def heroku_test_app_name
-    #   heroku_test_url[HEROKU_GIT_REGEX, 1]
-    # end
-    # 
-    # def heroku_test_url
-    #   response = RestClient.get "http://zenslap.me/heroku/resources/#{zenslap_id}.json"
-    #   JSON.parse( response.body )["heroku_url"]    
-    # end
-    # 
-    # def heroku_app
-    #   heroku_url[HEROKU_GIT_REGEX, 1]
-    # end
-    # 
-    # def heroku_url
-    #   git_urls.find{ |url| url =~ HEROKU_GIT_REGEX }            
-    # end
-    # 
-    # def github_url
-    #   git_urls.find{ |url| url =~ /git@github.com:.*/} || (raise "No github address found")
-    # end
-    # 
-    # def git_urls
-    #   git_repo.remotes.map{ |r| r.url }
-    # end
-    # 
-    # 
-    # private
-    # def git_repo
-    #   Git.open '.'
-    # end
-    # 
-    # def heroku_client
-    #   Heroku::Client.new *credentials
-    # end
-    # 
-    # def credentials
-    #   Heroku::Command::Auth.new(nil).get_credentials
     # end
   end
 end
