af397cb4bd4bdaf3b044038eddd31fedb4cce120
opsb <oliver@opsb.co.uk>
2010-11-07 00:08:48 +0000

    Updated /projects create call to zenslap

--- lib/zenslap/zenslap_client.rb
+++ lib/zenslap/zenslap_client.rb
@@ -1,15 +1,16 @@
 class ZenslapClient
   
-  def configure(id, repo_owner, repo_name, github_credentials, heroku_email, heroku_password)
+  def configure(uuid, repo_owner, repo_name, github_credentials, heroku_email, heroku_password)
     #TODO need to make https
-    RestClient.put("http://zenslap.me/heroku/resources/#{id}", 
+    RestClient.post("http://zenslap.me/projects", 
       {
         :project => github_credentials.merge({
                                               :owner => repo_owner, 
                                               :name => repo_name,
                                               :heroku_email => heroku_email,
-                                              :heroku_password => heroku_password}
-                                             )
+                                              :heroku_password => heroku_password,
+                                              :uuid => uuid
+                                             })
       }
     )
   end
--- test/zenslap/zenslap_client_test.rb
+++ test/zenslap/zenslap_client_test.rb
@@ -13,14 +13,25 @@ class ZenslapClientTest < Test::Unit::TestCase
     HEROKU_PASSWORD = "password"
     
     setup do
+      RestClient.stubs(:post)      
       @zenslap_client = ZenslapClient.new
     end
     
     should "configure the project" do
-      stub_request(:put, "http://zenslap.me/heroku/resources/#{ZENSLAP_ID}").
-                    with( { :body => {:project => GITHUB_CREDENTIALS.merge({:owner => OWNER, :name => NAME, :heroku_email => HEROKU_EMAIL, :heroku_password => HEROKU_PASSWORD})}} ).
-                    to_return(:status => 200)
       @zenslap_client.configure ZENSLAP_ID, OWNER, NAME, GITHUB_CREDENTIALS, HEROKU_EMAIL, HEROKU_PASSWORD
+      assert_received RestClient, :post do |expect|
+        expect.with( "http://zenslap.me/projects", 
+                      :project => GITHUB_CREDENTIALS.merge(
+                                    { 
+                                      :owner => OWNER, 
+                                      :name => NAME, 
+                                      :heroku_email => HEROKU_EMAIL, 
+                                      :heroku_password => HEROKU_PASSWORD, 
+                                      :uuid => ZENSLAP_ID 
+                                    }
+                                  )
+                   )
+      end
     end
 
   end
